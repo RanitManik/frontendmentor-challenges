0cf527924b19dbb535708043b5fa38009e5fc723
Ranit Manik <138437760+RanitManik@users.noreply.github.com>
2024-04-28 14:02:32 +0530

    added challenges

---
+++ FrontendMentor20—fylo-landing-page-with-two-column-layout/tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import("tailwindcss").Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        // Primary Colors
+        "very-dark-blue": "hsl(243, 87%, 12%)",
+        "desaturated-blue": "hsl(238, 22%, 44%)",
+        // Accent Colors
+        "bright-blue": "hsl(224, 93%, 58%)",
+        "moderate-cyan": "hsl(170, 45%, 43%)",
+        // Neutral Colors
+        "light-grayish-blue": "hsl(240, 75%, 98%)",
+        "light-gray": "hsl(0, 0%, 75%)",
+      },
+      typography: {
+        // Body Copy
+        DEFAULT: {
+          css: {
+            fontSize: "16px",
+          },
+        },
+        // Fonts
+        fontFamily: {
+          raleway: ["Raleway", "sans-serif"],
+          openSans: ["Open Sans", "sans-serif"],
+        },
+        fontWeight: {
+          raleway: {
+            400: "400",
+            700: "700",
+          },
+          openSans: {
+            400: "400",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
