756172fe5475dbd80208185e743e421b4e770ce1
Ranit Manik <138437760+RanitManik@users.noreply.github.com>
2024-05-01 01:33:29 +0530

    added challenges

--- FrontendMentor21—huddle-landing-page-with-curved-sections/tailwind.config.js
+++ FrontendMentor21—huddle-landing-page-with-curved-sections/tailwind.config.js
@@ -1,9 +1,27 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
-  content: ["*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: ["prettier-plugin-tailwindcss"],
-}
+    content: ["*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                pink: "hsl(322, 100%, 66%)",
+                "light-pink": "hsl(321, 100%, 78%)",
+                "light-red": "hsl(0, 100%, 63%)",
+                "very-dark-cyan": "hsl(192, 100%, 9%)",
+                "very-pale-blue": "hsl(207, 100%, 98%)",
+            },
+            // Fonts
+            fontFamily: {
+                Poppins: ["Poppins", "sans-serif"],
+                openSans: ['"Open Sans"', "sans-serif"],
 
+            },
+            screens: {
+                mobile: {max: "768px"},
+                tablet: {max: "1024px"},
+                laptop: {min: "1025px"},
+            },
+        },
+    },
+    plugins: [],
+};
