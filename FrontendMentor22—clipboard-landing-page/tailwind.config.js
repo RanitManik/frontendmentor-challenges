c601d1822f1afd1f2a4d7b7184fc9fb8e3812a81
RanitManik <138437760+RanitManik@users.noreply.github.com>
2024-05-01 19:43:06 +0530

    added challenges

--- FrontendMentor22—clipboard-landing-page/tailwind.config.js
+++ FrontendMentor22—clipboard-landing-page/tailwind.config.js
@@ -1,9 +1,26 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
-  content: [],
+  content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-strong-cyan": "hsl(171, 66%, 44%)",
+        "primary-light-blue": "hsl(233, 100%, 69%)",
+        "neutral-dark-grayish-blue": "hsl(210, 10%, 33%)",
+        "neutral-grayish-blue": "hsl(201, 11%, 66%)"
+      },
+      fontSize: {
+        body: "18px"
+      },
+      fontFamily: {
+        body: ["\"Bai Jamjuree\"", "sans-serif"]
+      },
+      fontWeight: {
+        normal: 400,
+        semibold: 600
+      }
+    }
   },
-  plugins: [],
-}
-
+  variants: {},
+  plugins: []
+};
